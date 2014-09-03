1b6027f2c46aeeb8ffb79f8e37d5dfcda91062a0
Pat Cavit <pcavit@gmail.com>
2014-09-02 23:16:06 -0700

    Give browse something slightly more interesting to do

--- app/pages/browse/browse.js
+++ app/pages/browse/browse.js
@@ -1,17 +1,21 @@
 /*global m */
+"use strict";
 
 import page from "../../structure";
+import sidebar from "../../components/browse-sidebar";
 
 var browse = {
-    controller : function() {
-    },
+    controller : function() {},
     
     view : function(ctrl) {
-        return page(browse.content(ctrl));
+        return page(
+            sidebar.view(new sidebar.controller()),
+            browse.content(ctrl)
+        );
     },
 
     content : function(ctrl) {
-        return m(".browse", "BROWSE");
+        return m(".browse", "ITEMS 'N SHIT");
     }
 };
 
